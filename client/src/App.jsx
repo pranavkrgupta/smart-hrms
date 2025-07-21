cae9d429613e683a4e4ff1b927b96ecbc636b4d5
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2025-07-21 11:53:16 +0530

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'
-
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
