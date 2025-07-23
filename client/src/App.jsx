9c1d3b5a43174960f3566e275b5c5f1156f61125
Jainam Shah <jainam1228@gmail.com>
2025-07-23 17:33:14 +0530

    File added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login'
-import Leave from './pages/Employee/Leave';
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Leave from "./pages/Employee/Leave";
 import Settings from "./Pages/Employee/Settings";
+import SalaryDetails from "./pages/Employee/SalaryDetails";
+import HRMSDashboard from "./pages/Employee/SalaryDetails";
 
 function App() {
   return (
@@ -9,8 +11,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/Employee/Leave" element={<Leave />} />
       <Route path="/Employee/Settings" element={<Settings />} />
+      <Route path="/Employee/SalaryDetails" element={<SalaryDetails />} />
     </Routes>
-  )
+  );
 }
 
 export default App;
