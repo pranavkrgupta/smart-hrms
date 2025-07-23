d9f76a88cdc7567ae62e7740eea3bdcdd965fbbc
Jainam Shah <jainam1228@gmail.com>
2025-07-23 17:51:42 +0530

    Updated App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,26 @@ import Login from "./pages/Login";
 import Leave from "./pages/Employee/Leave";
 import Settings from "./Pages/Employee/Settings";
 import SalaryDetails from "./pages/Employee/SalaryDetails";
-import HRMSDashboard from "./pages/Employee/SalaryDetails";
 
 function App() {
+  // Salary Data
+  const salaryData = {
+    basicSalary: 40000,
+    hra: 7000,
+    grossSalary: 48000,
+    pfDeduction: 4800,
+    netSalary: 43200,
+  };
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/Employee/Leave" element={<Leave />} />
       <Route path="/Employee/Settings" element={<Settings />} />
-      <Route path="/Employee/SalaryDetails" element={<SalaryDetails />} />
+      // Passing salary Details as a Props
+      <Route
+        path="/Employee/SalaryDetails"
+        element={<SalaryDetails {...salaryData} />}
+      />
     </Routes>
   );
 }
