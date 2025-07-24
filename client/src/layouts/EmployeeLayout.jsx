986d02e47c562d7dffd4b03f461c924125b0ec10
Jainam Shah <jainam1228@gmail.com>
2025-07-24 21:57:17 +0530

    Done changes in EmployeeLayout

--- client/src/layouts/EmployeeLayout.jsx
+++ client/src/layouts/EmployeeLayout.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const employeeLinks = [
-    // { label: "Dashboard", path: "/employee/dashboard" },
-    { label: "Leave", path: "/employee/leave" },
-    { label: "Attendance", path: "/employee/attendance" },
-    // { label: "My Profile", path: "/employee/profile" },
-    { label: "My Salary", path: "/employee/salaryDetails" },
-    { label: "Settings", path: "/employee/settings" },
-  ];
+  { label: "Dashboard", path: "/employee/dashboard" },
+  { label: "Leave", path: "/employee/leave" },
+  { label: "Attendance", path: "/employee/attendance" },
+  // { label: "My Profile", path: "/employee/profile" },
+  { label: "My Salary", path: "/employee/salaryDetails" },
+  { label: "Settings", path: "/employee/settings" },
+];
 function EmployeeLayout() {
   return (
     <div className="flex">
@@ -18,7 +18,7 @@ function EmployeeLayout() {
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
-export default EmployeeLayout
+export default EmployeeLayout;
