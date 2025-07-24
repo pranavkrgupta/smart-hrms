281b2367a3bb9cc74de0eab115659fd52ceed6e4
Jainam Shah <jainam1228@gmail.com>
2025-07-24 21:01:28 +0530

    Dashboard page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Leave from './pages/Employee/Leave';
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Leave from "./pages/Employee/Leave";
 import Settings from "./pages/Employee/Settings";
-import Attendance from "./pages/Employee/Attendance"
-import SalaryDetails from "./pages/Employee/SalaryDetails"
-import ManageEmployees from './pages/admin/ManageEmployee';
-import LeaveManagement from './pages/admin/LeaveManagement';
-import AttendanceManagement from './pages/admin/AttendanceManagement';
-import ManageDepartments from './pages/admin/ManageDepartment';
-import AdminLayout from './layouts/AdminLayout';
-import EmployeeLayout from './layouts/EmployeeLayout';
-
+import Attendance from "./pages/Employee/Attendance";
+import SalaryDetails from "./pages/Employee/SalaryDetails";
+import ManageEmployees from "./pages/admin/ManageEmployee";
+import LeaveManagement from "./pages/admin/LeaveManagement";
+import AttendanceManagement from "./pages/admin/AttendanceManagement";
+import ManageDepartments from "./pages/admin/ManageDepartment";
+import AdminLayout from "./layouts/AdminLayout";
+import EmployeeLayout from "./layouts/EmployeeLayout";
+import Dashboard from "./pages/Employee/Dashboard";
 
 function App() {
   return (
@@ -25,11 +25,12 @@ function App() {
       </Route>
 
       {/* Employee Route */}
-      <Route path="/employee" element={<EmployeeLayout />} >
+      <Route path="/employee" element={<EmployeeLayout />}>
         <Route path="leave" element={<Leave />} />
         <Route path="settings" element={<Settings />} />
         <Route path="salaryDetails" element={<SalaryDetails />} />
         <Route path="attendance" element={<Attendance />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   );
