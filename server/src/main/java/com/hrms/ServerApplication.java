81cb9a1c2905f65021694b3710a35e925aba01e8
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2025-08-05 19:28:21 +0530

    ModelMapper added

--- server/src/main/java/com/hrms/ServerApplication.java
+++ server/src/main/java/com/hrms/ServerApplication.java
@@ -1,7 +1,9 @@
 package com.hrms;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +11,10 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
+	
+	@Bean
+	ModelMapper modelMapper() {
+		System.out.println("Creating Model Mapper");
+		return new ModelMapper();
+	}
 }
