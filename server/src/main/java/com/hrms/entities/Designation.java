8d6bd8ec1986b435ab0cb16adf6a7240d7eb692f
Nishant Dwivedi <nishant.dwivedi@hotmail.com>
2025-08-05 14:52:49 +0530

    added unique constraint for  department_id + designation_name

--- server/src/main/java/com/hrms/entities/Designation.java
+++ server/src/main/java/com/hrms/entities/Designation.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -19,20 +21,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+
+// we are using a surrogate key (designation_id) for simplicity, that does not mean multiple rows with the same designation_name AND department_name can be allowed to exist simultaneously. Ideally, this would be the composite PK enforcing uniqueness implicitly. When using surrogates, this needs to be done manually.
+@Table(name = "designation", uniqueConstraints = { @UniqueConstraint(columnNames = { "name", "department_id" }) })
 public class Designation extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long departmentId;
 
 	@ManyToOne
-	@JoinColumn(nullable = false)
+// 	Foreign Key + NOT NULL enforces complete participation when relation is implicit. FK by itself won't enforce complete participation as it can contain NULL values.    
+	@JoinColumn(name = "department_id", nullable = false)
 	private Department department;
 
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false)
 	@NotBlank
 	@Size(max = 100, message = "Designation size cannot exceed 100 characters.")
 	private String name;
-	
+
 	private String description;
 
 }
