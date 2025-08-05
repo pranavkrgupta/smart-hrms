633bf8fd0aa8e4c387948a7cc830b02f23bb49b1
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2025-08-05 15:49:27 +0530

    Update Designation.java

--- server/src/main/java/com/hrms/entities/Designation.java
+++ server/src/main/java/com/hrms/entities/Designation.java
@@ -2,6 +2,7 @@ package com.hrms.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,23 +24,36 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 
-// we are using a surrogate key (designation_id) for simplicity, that does not mean multiple rows with the same designation_name AND department_name can be allowed to exist simultaneously. Ideally, this would be the composite PK enforcing uniqueness implicitly. When using surrogates, this needs to be done manually.
+/*
+ * we are using a surrogate key (designation_id) for simplicity, that does not
+ * mean multiple rows with the same designation_name AND department_name can be
+ * allowed to exist simultaneously. Ideally, this would be the composite PK
+ * enforcing uniqueness implicitly. When using surrogates, this needs to be done
+ * manually.
+ */
+
 @Table(name = "designation", uniqueConstraints = { @UniqueConstraint(columnNames = { "name", "department_id" }) })
 public class Designation extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long departmentId;
+	private Integer designationId;
 
-	@ManyToOne
-// 	Foreign Key + NOT NULL enforces complete participation when relation is implicit. FK by itself won't enforce complete participation as it can contain NULL values.    
+	@NotBlank(message = "Designation name cannot be blank")
+	@Column(name = "name", nullable = false, length = 100)
+	private String name;
+
+	/*
+	 * Foreign Key + NOT NULL enforces complete participation when relation is
+	 * implicit. FK by itself won't enforce complete participation as it can contain
+	 * NULL values.
+	 */
+	
+	@NotNull(message = "Department is required")
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "department_id", nullable = false)
 	private Department department;
 
-	@Column(nullable = false)
-	@NotBlank
-	@Size(max = 100, message = "Designation size cannot exceed 100 characters.")
-	private String name;
-
+	@Size(max = 255, message = "Description must be at most 255 characters")
 	private String description;
 
 }
