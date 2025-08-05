9e8de35cbce67ca421daf49ebe4b78f58d93fdc2
varun <varunkm192002@gmail.com>
2025-08-05 14:33:35 +0530

    Added server side validations in Attendance and Leave entity

--- server/src/main/java/com/hrms/entities/Attendance.java
+++ server/src/main/java/com/hrms/entities/Attendance.java
@@ -1,13 +1,9 @@
 package com.hrms.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import java.time.*;
-
-
-
-import jakarta.persistence.Entity;
-
 
 @Getter
 @Setter
@@ -21,10 +17,12 @@ public class Attendance extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "User is required")
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @NotNull(message = "Date is required")
     @Column(nullable = false)
     private LocalDate date;
 
@@ -32,11 +30,10 @@ public class Attendance extends BaseEntity {
 
     private LocalTime checkOut;
 
+    @Min(value = 0, message = "Duration must be 0 or more minutes")
     private Integer durationInMinutes;
 
+    @NotNull(message = "Status is required")
     @Enumerated(EnumType.STRING)
     private AttendanceStatus status;
-
- 
 }
-
--- server/src/main/java/com/hrms/entities/Leaves.java
+++ server/src/main/java/com/hrms/entities/Leaves.java
@@ -1,6 +1,7 @@
 package com.hrms.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import java.time.*;
 
@@ -10,8 +11,7 @@ import java.time.*;
 @AllArgsConstructor
 @Entity
 @ToString
-
-public class Leaves extends BaseEntity{
+public class Leaves extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,25 +19,34 @@ public class Leaves extends BaseEntity{
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @NotNull(message = "User must not be null")
     private User user;
 
     @Column(nullable = false)
+    @NotNull(message = "From date is required")
+    @FutureOrPresent(message = "From date cannot be in the past")
     private LocalDate fromDate;
 
     @Column(nullable = false)
+    @NotNull(message = "To date is required")
+    @FutureOrPresent(message = "To date cannot be in the past")
     private LocalDate toDate;
 
     @Column(nullable = false)
+    @NotBlank(message = "Reason cannot be blank")
+    @Size(min = 5, max = 255, message = "Reason must be between 5 and 255 characters")
     private String reason;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
+    @NotNull(message = "Leave status is required")
     private LeaveStatus status;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
+    @NotNull(message = "Leave type is required")
     private LeaveType type;
 
+    @Size(max = 500, message = "Comment can be up to 500 characters")
     private String comment;
-
 }
