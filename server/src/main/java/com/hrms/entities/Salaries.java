24f2f3e18eb84ec49d9f0409243dd18f995d2f82
Jainam Shah <jainam1228@gmail.com>
2025-08-05 14:53:17 +0530

    Updated the Entity and done necessary changes

--- server/src/main/java/com/hrms/entities/Salaries.java
+++ server/src/main/java/com/hrms/entities/Salaries.java
@@ -2,21 +2,25 @@ package com.hrms.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+import java.math.BigDecimal;
 import java.time.*;
 
 @Getter
@@ -24,6 +28,10 @@ import java.time.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "Salaries", uniqueConstraints = {
+	    @UniqueConstraint(columnNames = {"user_id", "applicable_from"})
+	})
+
 public class Salaries extends BaseEntity {
 	
 	@Id
@@ -31,20 +39,21 @@ public class Salaries extends BaseEntity {
 	private Long id;
 	
 	@NotNull(message = "User must not be null")
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name="user_id",nullable=false)
 	private User user;
 	
-    @NotNull(message = "Amount must not be null")
-    @Positive(message = "Amount must be positive")
-	@Column(nullable=false)
-	private Long amount;
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.00", inclusive = true, message = "Amount must be non-negative")
+	@Column(nullable=false,precision = 10, scale = 2)
+	private BigDecimal amount;
 	
-	@NotNull(message = "Applicable date must not be null")
-	@Column(nullable=false)
+	@NotNull(message = "Applicable from date is required")
+	@Column(name="applicable_from",nullable=false)
 	private LocalDate applicableFrom;
 	
-	@PositiveOrZero(message = "PF deduction must be zero or positive")
-	private Long pfDeduction;
+    @DecimalMin(value = "0.00", inclusive = true, message = "PF Deductions must be non-negative")
+    @Column(name = "pf_deductions", precision = 10, scale = 2)
+	private BigDecimal pfDeduction=BigDecimal.ZERO;
 	
 }
