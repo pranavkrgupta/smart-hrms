cafa3338aa8f3a93b9f120ced23d87184277a2a1
Nishant Dwivedi <nishant.dwivedi@hotmail.com>
2025-08-05 14:06:14 +0530

    added Designation entity

--- server/src/main/java/com/hrms/entities/Designation.java
+++ server/src/main/java/com/hrms/entities/Designation.java
@@ -1,9 +1,14 @@
 package com.hrms.entities;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +21,18 @@ import lombok.Setter;
 @Entity
 public class Designation extends BaseEntity {
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long departmentId;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long departmentId;
 
-    @Column(nullable = false, unique = true)
-    private String name;
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Department department;
 
-    private String description;   
+	@Column(nullable = false, unique = true)
+	@NotBlank
+	@Size(max = 100, message = "Designation size cannot exceed 100 characters.")
+	private String name;
+	
+	private String description;
 
 }
